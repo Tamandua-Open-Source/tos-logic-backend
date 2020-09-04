86aed67cd706712149ed73ef06508d3f4e3d7607
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2020-09-04 11:39:14 -0300

    Add application layer

---
+++ api/lib/application/use-cases/users/index.js
@@ -0,0 +1,9 @@
+import GetUserUseCase from './get-user-use-case'
+import GetUserPreferencesUseCase from './get-user-preferences-use-case'
+import PatchUserPreferencesUseCase from './patch-user-preferences-use-case'
+
+export {
+  GetUserUseCase,
+  GetUserPreferencesUseCase,
+  PatchUserPreferencesUseCase,
+}
