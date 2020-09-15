ce07fd400df3c9a79808b30564556140f496e4a9
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2020-09-15 10:36:30 -0300

    Add scheduling facade on interfaces layer

--- api/lib/interfaces/controllers/composers/timer-controller-composer.js
+++ api/lib/interfaces/controllers/composers/timer-controller-composer.js
@@ -1,6 +1,9 @@
 import TimerController from '../timer-controller'
 import UserRepository from '../../../infrastructure/repositories/user-repository'
 import StateMachineFacade from '../../../infrastructure/facades/state-machine-facade'
+import IdleSystemFacade from '../../../infrastructure/facades/idle-system-facade'
+import SchedulingFacade from '../../../infrastructure/facades/scheduling-facade'
+import FirebaseAdminFacade from '../../../infrastructure/firebase/firebase-admin-facade'
 
 import {
   StartTimerUseCase,
@@ -11,38 +14,48 @@ import {
   ResumeTimerUseCase,
 } from '../../../application/use-cases/timer'
 
-//injetar:
-//queue facade
-//firebase admin facade
-//internal action facade
 class TimerControllerComposer {
   static compose() {
+    const firebaseAdminFacade = new FirebaseAdminFacade()
     const userRepository = new UserRepository()
     const stateMachineFacade = new StateMachineFacade()
+    const idleSystemFacade = new IdleSystemFacade({
+      userRepository,
+    })
+    const schedulingFacade = new SchedulingFacade({
+      firebaseAdminFacade,
+      idleSystemFacade,
+    })
 
     const startTimerUseCase = new StartTimerUseCase({
       userRepository,
       stateMachineFacade,
+      schedulingFacade,
     })
     const finishTimerUseCase = new FinishTimerUseCase({
       userRepository,
       stateMachineFacade,
+      schedulingFacade,
     })
     const workTimerUseCase = new WorkTimerUseCase({
       userRepository,
       stateMachineFacade,
+      schedulingFacade,
     })
     const breakTimerUseCase = new BreakTimerUseCase({
       userRepository,
       stateMachineFacade,
+      schedulingFacade,
     })
     const pauseTimerUseCase = new PauseTimerUseCase({
       userRepository,
       stateMachineFacade,
+      schedulingFacade,
     })
     const resumeTimerUseCase = new ResumeTimerUseCase({
       userRepository,
       stateMachineFacade,
+      schedulingFacade,
     })
 
     return new TimerController({
