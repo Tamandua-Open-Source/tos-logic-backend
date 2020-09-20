ff3ee811ce271f92aefce59d25e220cad6bd8ef0
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2020-09-20 18:33:30 -0300

    Make timer use-cases responsible only for calling state machine methods

--- api/lib/application/use-cases/timer/pause-timer-use-case.js
+++ api/lib/application/use-cases/timer/pause-timer-use-case.js
@@ -1,52 +1,10 @@
 class PauseTimerUseCase {
-  constructor({ userRepository, stateMachineFacade, schedulingFacade }) {
-    this.userRepository = userRepository
+  constructor({ stateMachineFacade }) {
     this.stateMachineFacade = stateMachineFacade
-    this.schedulingFacade = schedulingFacade
   }
 
   async execute(userId) {
-    //DEBUG: RESET TO TRIGGABLE STATE
-    // await this.userRepository.patchUserPreferences(userId, {
-    //   lastState: 'BREAK',
-    //   currentState: 'WORK',
-    // })
-
-    const preferences = await this.userRepository.getUserPreferences(userId)
-
-    if (!preferences) {
-      return null
-    }
-
-    if (!this.stateMachineFacade.canPauseFrom(preferences.currentState)) {
-      return null
-    }
-
-    this.schedulingFacade.removeAllScheduledPushNotifications({ userId })
-    this.schedulingFacade.removeAllScheduledIdleSystemActions({ userId })
-
-    const patchedPreferences = await this.userRepository.patchUserPreferences(
-      userId,
-      {
-        lastPauseStartTime: new Date(),
-        lastState: preferences.currentState,
-        currentState: this.stateMachineFacade.onPause(),
-      }
-    )
-
-    this.schedulingFacade.schedulePauseIdleAction({
-      userId,
-      fcmToken: preferences.fcmToken,
-      delay: preferences.pauseLimitDuration,
-      delayToInactive:
-        preferences.pauseLimitDuration + preferences.pauseIdleLimitDuration,
-      delayStartCycle: 35000, //calcular
-    })
-
-    return {
-      from: patchedPreferences.lastState,
-      to: patchedPreferences.currentState,
-    }
+    return await this.stateMachineFacade.onPause({ userId })
   }
 }
 
