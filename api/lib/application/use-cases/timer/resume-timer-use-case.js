f10649073ec5dd8c0a621e0243d4bf3dd3442521
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2020-09-20 11:48:51 -0300

    Add elapsed duration to pause/resume

--- api/lib/application/use-cases/timer/resume-timer-use-case.js
+++ api/lib/application/use-cases/timer/resume-timer-use-case.js
@@ -5,14 +5,6 @@ class ResumeTimerUseCase {
     this.schedulingFacade = schedulingFacade
   }
 
-  dateDiffInDays(a, b) {
-    const _MS_PER_DAY = 1000 * 60 * 60 * 24
-    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate())
-    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate())
-
-    return Math.floor((utc2 - utc1) / _MS_PER_DAY)
-  }
-
   async execute(userId) {
     //DEBUG: RESET TO TRIGGABLE STATE
     // await this.userRepository.patchUserPreferences(userId, {
@@ -43,18 +35,23 @@ class ResumeTimerUseCase {
     )
 
     if (preferences.lastState === this.stateMachineFacade.workState) {
-      const lastWorkStartTime = new Date.parse(
-        preferences.lastWorkStartTime
-      ).getTime()
-      const lastPauseStartTime = new Date.parse(
-        preferences.lastPauseStartTime
-      ).getTime()
-      const elapsedDuration = (lastWorkStartTime = lastPauseStartTime)
+      const lastWorkStartTime = new Date(preferences.lastWorkStartTime)
+      const lastPauseStartTime = new Date(preferences.lastPauseStartTime)
+      const elapsedDuration =
+        lastWorkStartTime.getMilliseconds() -
+        lastPauseStartTime.getMilliseconds()
+
+      console.log(
+        '[DEBUG] - WORK_STATE - elapsed duration: ',
+        elapsedDuration,
+        'ms',
+        lastWorkStartTime
+      )
 
       this.schedulingFacade.scheduleNextBreakNotification({
         userId,
         fcmToken: preferences.fcmToken,
-        delay: 5000 ?? preferences.workDuration - elapsedDuration,
+        delay: preferences.workDuration - elapsedDuration,
       })
 
       this.schedulingFacade.scheduleWorkIdleAction({
@@ -62,20 +59,23 @@ class ResumeTimerUseCase {
         fcmToken: preferences.fcmToken,
         delay: 15000 ?? preferences.workLimitDuration - elapsedDuration,
         delayToInactive:
-          25000 ??
           preferences.workLimitDuration +
-            preferences.workIdleLimitDuration -
-            elapsedDuration,
+          preferences.workIdleLimitDuration -
+          elapsedDuration,
         delayStartCycle: 35000, //calcular
       })
     } else if (preferences.lastState === this.stateMachineFacade.breakState) {
-      const lastBreakStartTime = new Date.parse(
-        preferences.lastBreakStartTime
-      ).getTime()
-      const lastPauseStartTime = new Date.parse(
-        preferences.lastPauseStartTime
-      ).getTime()
-      const elapsedDuration = (lastBreakStartTime = lastPauseStartTime)
+      const lastBreakStartTime = new Date(preferences.lastBreakStartTime)
+      const lastPauseStartTime = new Date(preferences.lastPauseStartTime)
+      const elapsedDuration =
+        lastBreakStartTime.getMilliseconds() -
+        lastPauseStartTime.getMilliseconds()
+
+      console.log(
+        '[DEBUG] - BREAK_STATE - elapsed duration: ',
+        elapsedDuration,
+        'ms'
+      )
 
       this.schedulingFacade.scheduleNextWorkNotification({
         userId,
