3eb5c6bcb80437518cc670b1c519cc736c50761c
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2020-10-02 10:56:54 -0300

    Update swagger doc

--- api/lib/application/use-cases/timer-preferences/create-timer-preferences-use-case.js
+++ api/lib/application/use-cases/timer-preferences/create-timer-preferences-use-case.js
@@ -4,6 +4,14 @@ class CreateTimerPreferencesUseCase {
   }
 
   async execute(userId) {
+    const preferences = await this.timerPreferencesRepository.getTimerPreferences(
+      userId
+    )
+
+    if (preferences) {
+      return null
+    }
+
     return await this.timerPreferencesRepository.createTimerPreferences(userId)
   }
 }
